44c97a65fd29c61f67aa9b0b1df9e54c77d05aa4
Rekabriyan <rekabriyanch08@gmail.com>
2024-07-01 16:55:02 +0700

    inisialisasi project

---
+++ resources/views/about.blade.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Halaman About</title>
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.">
+    <link rel="stylesheet" href="public\css">
+    <script src="public\js\script.js"></script>
+</head>
+<body>
+    <a href="/">Home</a>
+    <a href="/about">About</a>
+    <a href="/contact">Contact</a>
+    <a href="/blog">Blog</a>
+
+    <h1>Halaman About</h1>
+    <h3>Owner: {{ $nama }}</h3>
+</body>
+</html>
---
+++ resources/views/contact.blade.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Halaman Contact</title>
+</head>
+<body>
+    <a href="/">Home</a>
+    <a href="/about">About</a>
+    <a href="/contact">Contact</a>
+    <a href="/blog">Blog</a>
+    <h1>Halaman Contact</h1>
+    <ul>
+        <li>
+            Instagram: <a href="https://instagram.com/rekabriyan_">Instagram</a>
+        </li>
+        <li>
+            Twitter: <a href="https://twitter.com/rekabriyan_">Twitter</a>
+        </li>
+        <li>
+            Facebook: <a href="https://www.facebook.com/rekabriyan">Facebook</a>
+        </li>
+    </ul>
+</body>
+</html>
